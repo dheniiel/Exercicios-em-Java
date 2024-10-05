833edf95def06eee802be72b019ed8bdecbed9df
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2024-10-04 22:36:19 -0300

    Colocando scanners

--- exercicios2/src/main/java/lista2/Teste.java
+++ exercicios2/src/main/java/lista2/Teste.java
@@ -5,12 +5,20 @@ public class Teste {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
+        System.out.println("Digite o nome do funcionário: ");
         String nome = scan.nextLine();
+        
+        System.out.println("Digite a função do funcionário: ");
         String funcao = scan.nextLine();
+        
+        System.out.println("Digite o salario do funcionário: ");
         double salario = scan.nextDouble();
         
         Funcionario funcionarioUm = new Funcionario(nome, funcao, salario);
         
-        
+        System.out.println("Digite a promoção desejada do funcionário: ");
+        double promocao = scan.nextDouble();
+        funcionarioUm.promoverSalario(promocao);
+
     }
 }
