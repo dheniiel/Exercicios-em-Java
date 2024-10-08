80578c6173317c7f9342d0acb9ec45c64daa7392
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2024-10-07 21:48:50 -0300

    Exercício sugestão de email

---
+++ exercicios2/src/main/java/lista1/Email.java
@@ -0,0 +1,14 @@
+package lista1;
+import java.util.Scanner;
+import java.lang.String;
+
+public class Email {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite seu nome: ");
+        String nomeUsu = scan.nextLine();
+      
+        System.out.println(nomeUsu.trim().replaceAll("\\s+", " ").toLowerCase() + "@gmail.com");
+    }
+}
